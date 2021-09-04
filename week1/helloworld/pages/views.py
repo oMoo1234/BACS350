0c0b434c9c21a2340bfd4c55e20d9a2d99353ed6
O Moo Gay <Omoog10101@gmail.com>
2021-09-03 21:59:19 -0600

    did half of week2 lesson

--- week1/helloworld/pages/views.py
+++ week1/helloworld/pages/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def homePageView(request):
     return HttpResponse('Hello, World!')
 
 # Create your views here.
+# hello world
