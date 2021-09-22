47832617e80557103dc10feb13a763bbe4a223a0
O Moo Gay <Omoog10101@gmail.com>
2021-09-22 13:23:20 -0600

    fix static code

--- week4.3/Superhero/config/settings.py
+++ week4.3/Superhero/config/settings.py
@@ -118,19 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-TEMPLATES = [
-    {
-
-        'DIRS': ['templates'],
-
-    },
-]
-
 
 # Enable the static media server (Images, CSS, Javascript)
-
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
