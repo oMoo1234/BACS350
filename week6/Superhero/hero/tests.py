3f211bb7ded75e11fe8ba0a2a668c847c9b023f5
O Moo Gay <Omoog10101@gmail.com>
2021-10-03 21:37:37 -0600

    created week6

---
+++ week6/Superhero/hero/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+# class AdminTests(SimpleTestCase):
+
+    def test_admin_page_status_code(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
