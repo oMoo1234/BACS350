99112538ae5fb3bb33a7fc2b60c200334e4ef1a2
O Moo Gay <Omoog10101@gmail.com>
2021-10-08 16:55:26 -0600

    week7 created

---
+++ week7/Superhero/config/urls.py
@@ -0,0 +1,30 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from hero.views import IndexPage, HeroListView, HeroDetailView, CreateHero, DeleteHero, UpdateHero
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexPage.as_view()),
+    path('hero/', HeroListView.as_view(), name="hero_list"),
+    path('hero/<int:pk>', HeroDetailView.as_view()),
+    path('hero/add', CreateHero.as_view(), name="add_hero"),
+    path('hero/<int:pk>/', UpdateHero.as_view(),  name='update_hero'),
+    path('hero/<int:pk>/delete', DeleteHero.as_view(), name="delete_hero")
+
+]
