8b477c30438d68444c754b9de7b2ef9a088b0790
O Moo Gay <Omoog10101@gmail.com>
2021-10-27 21:20:51 -0600

    added header footer navbar

--- week9/Authenticate/hero/models.py
+++ week9/Authenticate/hero/models.py
@@ -17,3 +17,22 @@ class Hero (models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('hero_list')
+
+
+class Chapter(models.Model):
+    book = models.CharField(max_length=200)
+    order = models.IntegerField()
+    title = models.CharField(max_length=200)
+
+    def export_record(self):
+        return [self.book, self.order, self.title]
+
+    @staticmethod
+    def import_record(values):
+        c = Chapter.objects.get_or_create(book=values[0], order=values[1])[0]
+        c.title = values[2]
+        c.save()
+        return c
+
+    def __str__(self):
+        return f'{ self.book.title} - {self.order} - {self.title}'
